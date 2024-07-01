6ffc9cc91f4e531f87dbb6cb660e5e7c7b5733b7
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-07-01 23:19:02 +0900

    0701 fix : alert 렌더링 두번되는 이슈 해결

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,15 +1,23 @@
-import { User } from '../models/user.model';
-import { httpClient } from './http';
+import { User } from "../models/user.model";
+import { httpClient } from "./http";
 
 export const signup = async (userData: User) => {
-  const response = await httpClient.post('/auth/join', userData);
-  console.log(response.data);
+    const response = await httpClient.post("/auth/join", userData);
+    console.log(response.data);
 };
 
 interface LoginResponse {
-  token: string; //로그인할때 만든 토큰
+    accessToken: string; //로그인할때 만든 토큰
 }
 export const login = async (data: User) => {
-  const response = await httpClient.post<LoginResponse>('/auth/login', data);
-  return response.data;
+    const response = await httpClient.post<LoginResponse>("/auth/login", data);
+    return response.data;
+};
+
+export const changepassword = async (data: User) => {
+    const response = await httpClient.post<LoginResponse>(
+        "/auth/changePassword",
+        data
+    );
+    return response.data;
 };
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,15 +1,17 @@
 export interface User {
-  email: string;
-  password: string;
-  nickname?: string;
+    email: string;
+    password: string;
+    nickname?: string;
 }
 
 export interface UserChangePassword extends User {
-  newpassword1: string;
-  newpassword2: string;
+    email: string;
+    password: string;
+    newpassword1: string;
+    newpassword2: string;
 }
 
 export interface UserInfo {
-  userNickname: string;
-  userGoal: string;
+    userNickname: string;
+    userGoal: string;
 }
