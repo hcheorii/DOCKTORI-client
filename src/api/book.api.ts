2ee968bc8d98c39443ec957e47ad2040855fc659
Eunji <eunjipark0129@gmail.com>
2024-07-03 14:59:12 +0900

    0703 fix : SearchModal 수정 (createPortal)

--- src/api/book.api.ts
+++ src/api/book.api.ts
@@ -1,23 +1,28 @@
-import { BookListItem } from "../models/book.model";
-import { httpClient } from "./http";
+import { BookListItem, BookSearchItem } from '../models/book.model';
+import { httpClient } from './http';
 
 interface fetchBookListResponse {
-    books: BookListItem[];
+  books: BookListItem[];
+}
+
+interface fetchSearchBookResponse {
+  books: BookSearchItem[];
 }
 interface fetchSearchBooks {
-    title: string;
+  title: string;
 }
 
 export const fetchBookList = async (url: string) => {
-    const { data } = await httpClient.get<fetchBookListResponse>(url);
-    return data;
+  const { data } = await httpClient.get<fetchBookListResponse>(url);
+  return data;
 };
 
 export const fetchSearchBookList = async (params: fetchSearchBooks) => {
-    const response = await httpClient.get<fetchBookListResponse>(
-        "/book/search",
-        { params } // 객체 형식으로 전달
-    );
-    console.log(params.title);
-    return response;
+  const response = await httpClient.get<fetchSearchBookResponse>(
+    '/book/search',
+    { params } // 객체 형식으로 전달
+  );
+  console.log(params.title);
+  console.log(response);
+  return response;
 };
