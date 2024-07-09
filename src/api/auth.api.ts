24babd37daeb2f92b95a9e598f5ca87eaba69fe2
Eunji <eunjipark0129@gmail.com>
2024-07-09 13:42:05 +0900

    0709 fix : 비밀번호 변경 api 호출 부분 수정

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,81 +1,78 @@
 import {
-    ChangeNickNameProps,
-    ChangeNickNameResponse,
-    LoginResponse,
-    LogoutInfo,
-    LogoutResponse,
-    SignupInfo,
-    User,
-    UserChangePassword,
-    UserGoalResponse,
-    UserNicknameProps,
-    UserNicknameResponse,
-} from "../models/user.model";
-import { httpClient } from "./http";
+  ChangeNickNameProps,
+  ChangeNickNameResponse,
+  LoginResponse,
+  LogoutInfo,
+  LogoutResponse,
+  SignupInfo,
+  User,
+  UserChangePassword,
+  UserGoalResponse,
+  UserNicknameProps,
+  UserNicknameResponse,
+} from '../models/user.model';
+import { httpClient } from './http';
 
 // 회원 가입 함수
 export const signup = async (userData: SignupInfo) => {
-    const response = await httpClient.post("/auth/join", userData);
-    console.log(response.data);
+  const response = await httpClient.post('/auth/join', userData);
+  console.log(response.data);
 };
 
 // 로그인 함수
 export const login = async (data: User) => {
-    const response = await httpClient.post<LoginResponse>("/auth/login", data);
-    return response.data;
+  const response = await httpClient.post<LoginResponse>('/auth/login', data);
+  return response.data;
 };
 
 //로그아웃 함수
 export const logout = async (data: LogoutInfo) => {
-    const response = await httpClient.post<LogoutResponse>(
-        "/auth/logout",
-        data
-    );
-    return response.data;
+  const response = await httpClient.post<LogoutResponse>('/auth/logout', data);
+  return response.data;
 };
 
 // 비밀번호 변경 함수
 export const changepassword = async (data: UserChangePassword) => {
-    const response = await httpClient.post<LoginResponse>(
-        "/auth/changePassword",
-        data
-    );
-    return response.data;
+  const response = await httpClient.patch<LoginResponse>(
+    '/auth/password',
+    data
+  );
+  return response.data;
 };
 
 // 회원 탈퇴 함수
 export const withdrawal = async (data: LoginResponse) => {
-    const response = await httpClient.delete<LoginResponse>(
-        "/auth/cancelAccount",
-        {
-            data: data, // 요청 본문에 데이터를 포함시킵니다.
-        }
-    );
-    return response.data;
+  const response = await httpClient.delete<LoginResponse>(
+    '/auth/cancelAccount',
+    {
+      data: data, // 요청 본문에 데이터를 포함시킵니다.
+    }
+  );
+  return response.data;
 };
 
 export const changeNickname = async (data: ChangeNickNameProps) => {
-    const response = await httpClient.post<ChangeNickNameResponse>(
-        "/home/changeGoal",
-        data
-    );
+  const response = await httpClient.post<ChangeNickNameResponse>(
+    '/home/changeGoal',
+    data
+  );
 
-    return response.data;
+  return response.data;
 };
 
 export const fetchNickname = async (params: UserNicknameProps) => {
-    const response = await httpClient.get<UserNicknameResponse>(
-        "/home/nickname",
-        {
-            params,
-        }
-    );
-    return response;
+  const response = await httpClient.get<UserNicknameResponse>(
+    '/home/nickname',
+    {
+      params,
+    }
+  );
+  return response;
 };
 
 export const fetchGoal = async (params: UserNicknameProps) => {
-    const response = await httpClient.get<UserGoalResponse>("/home/main", {
-        params,
-    });
-    return response;
+  const response = await httpClient.get<UserGoalResponse>('/home/main', {
+    params,
+  });
+  return response;
 };
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,162 +1,167 @@
-import { useAuthStore } from "../store/authStore";
-import { useAlert } from "./useAlert";
-import { useNavigate } from "react-router-dom";
+import { useAuthStore } from '../store/authStore';
+import { useAlert } from './useAlert';
+import { useNavigate } from 'react-router-dom';
 import {
-    changeNickname,
-    changepassword,
-    login,
-    logout,
-    signup,
-    withdrawal,
-} from "../api/auth.api";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+  changeNickname,
+  changepassword,
+  login,
+  logout,
+  signup,
+  withdrawal,
+} from '../api/auth.api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import {
-    ChangeNickNameProps,
-    ChangeNickNameResponse, // ChangeNickNameResponse를 import합니다.
-    SignupInfo,
-    User,
-    UserChangePassword,
-} from "../models/user.model";
-import { useState } from "react";
-import { fetchMainResponse } from "../models/main.model";
-import { fetchMain } from "../api/main.api";
-import { BookListItem } from "../models/book.model";
+  ChangeNickNameProps,
+  ChangeNickNameResponse, // ChangeNickNameResponse를 import합니다.
+  SignupInfo,
+  User,
+  UserChangePassword,
+} from '../models/user.model';
+import { useState } from 'react';
+import { fetchMainResponse } from '../models/main.model';
+import { fetchMain } from '../api/main.api';
+import { BookListItem } from '../models/book.model';
 
 export const useAuth = () => {
-    const {
-        storeLogin,
-        storeLogout,
-        userNickname,
-        userGoal,
-        fetchAndSetNickname,
-        fetchAndSetGoal,
-    } = useAuthStore();
-    const { showAlert } = useAlert();
-    const nav = useNavigate();
-    const queryClient = useQueryClient();
+  const {
+    storeLogin,
+    storeLogout,
+    userNickname,
+    userGoal,
+    fetchAndSetNickname,
+    fetchAndSetGoal,
+  } = useAuthStore();
+  const { showAlert } = useAlert();
+  const nav = useNavigate();
+  const queryClient = useQueryClient();
 
-    const [bookReading, setBookReading] = useState<BookListItem[]>([]);
-    const [bookFinished, setBookFinished] = useState<BookListItem[]>([]);
-    const [bookFinishedCount, setBookFinishedCount] = useState<number>(0);
-    const [bookReadingCount, setBookReadingCount] = useState<number>(0);
-    const [isReadingEmpty, setIsReadingEmpty] = useState<boolean>(true);
-    const [isFinishEmpty, setIsFinishEmpty] = useState<boolean>(true);
+  const [bookReading, setBookReading] = useState<BookListItem[]>([]);
+  const [bookFinished, setBookFinished] = useState<BookListItem[]>([]);
+  const [bookFinishedCount, setBookFinishedCount] = useState<number>(0);
+  const [bookReadingCount, setBookReadingCount] = useState<number>(0);
+  const [isReadingEmpty, setIsReadingEmpty] = useState<boolean>(true);
+  const [isFinishEmpty, setIsFinishEmpty] = useState<boolean>(true);
 
-    const getMainData = async (token: string) => {
-        try {
-            const res = await fetchMain({ token });
-            const data: fetchMainResponse = res.data;
+  const getMainData = async (token: string) => {
+    try {
+      const res = await fetchMain({ token });
+      const data: fetchMainResponse = res.data;
 
-            fetchAndSetNickname(token);
-            fetchAndSetGoal(token);
-            setBookReading(data.bookReading);
-            setBookFinished(data.bookFinished);
-            setBookReadingCount(data.bookReadingCount);
-            setBookFinishedCount(data.bookFinishedCount);
-            setIsReadingEmpty(data.bookReading.length === 0);
-            setIsFinishEmpty(data.bookFinished.length === 0);
-        } catch (error) {
-            showAlert("데이터를 가져오는 데에 실패했습니다.");
-            console.error(error);
-        }
-    };
+      fetchAndSetNickname(token);
+      fetchAndSetGoal(token);
+      setBookReading(data.bookReading);
+      setBookFinished(data.bookFinished);
+      setBookReadingCount(data.bookReadingCount);
+      setBookFinishedCount(data.bookFinishedCount);
+      setIsReadingEmpty(data.bookReading.length === 0);
+      setIsFinishEmpty(data.bookFinished.length === 0);
+    } catch (error) {
+      showAlert('데이터를 가져오는 데에 실패했습니다.');
+      console.error(error);
+    }
+  };
 
-    const userLogin = useMutation({
-        mutationFn: (data: User) => login(data),
-        onSuccess: (res) => {
-            storeLogin(res.accessToken);
-            showAlert("로그인이 성공했습니다.");
-            fetchAndSetNickname(res.accessToken);
-            fetchAndSetGoal(res.accessToken);
-            getMainData(res.accessToken);
-            nav("/");
-        },
-        onError: (error: any) => {
-            console.log(error);
-            showAlert(error.response.data.message);
-        },
-    });
+  const userLogin = (data: User) => {
+    login(data).then(
+      (res) => {
+        storeLogin(res.accessToken);
+        showAlert('로그인이 성공했습니다.');
+        fetchAndSetNickname(res.accessToken);
+        fetchAndSetGoal(res.accessToken);
+        getMainData(res.accessToken);
+        nav('/');
+      },
+      (error) => {
+        console.log(error);
+        showAlert(error.response.data.message);
+      }
+    );
+  };
 
-    const userSignup = useMutation({
-        mutationFn: (data: SignupInfo) => signup(data),
-        onSuccess: () => {
-            showAlert("회원가입이 완료되었습니다.");
-            nav("/auth/login");
-        },
-        onError: (error: any) => {
-            console.log(error);
-            showAlert(error.response.data.message);
-        },
-    });
+  const userSignup = (data: SignupInfo) => {
+    signup(data).then(
+      () => {
+        showAlert('회원가입이 완료되었습니다.');
+        nav('/auth/login');
+      },
+      (error) => {
+        console.log(error);
+        showAlert(error.response.data.message);
+      }
+    );
+  };
 
-    const userLogout = useMutation({
-        mutationFn: (token: string) => logout({ accessToken: token }),
-        onSuccess: () => {
-            storeLogout();
-            showAlert("로그아웃이 완료되었습니다.");
-            nav("/auth/login");
-        },
-        onError: (error: any) => {
-            console.log(error);
-            showAlert(error.response.data.message);
-        },
-    });
+  const userLogout = (token: string) => {
+    logout({ accessToken: token }).then(
+      () => {
+        storeLogout();
+        showAlert('로그아웃이 완료되었습니다.');
+        nav('/auth/login');
+      },
+      (error) => {
+        console.log(error);
+        showAlert(error.response.data.message);
+      }
+    );
+  };
 
-    const userChangePassword = useMutation({
-        mutationFn: (data: UserChangePassword) => changepassword(data),
-        onSuccess: () => {
-            showAlert("비밀번호 변경이 완료되었습니다.\n다시 로그인 하세요.");
-            localStorage.removeItem("token");
-            nav("/auth/login");
-        },
-        onError: (error: any) => {
-            console.log(error);
-            showAlert(error.response.data.message);
-        },
-    });
+  const userChangePassword = (data: UserChangePassword) => {
+    changepassword(data).then(
+      () => {
+        showAlert('비밀번호 변경이 완료되었습니다.\n다시 로그인 하세요.');
+        localStorage.removeItem('token');
+        nav('/auth/login');
+      },
+      (error) => {
+        console.log(error);
+        showAlert(error.response.data.message);
+      }
+    );
+  };
 
-    const userWithdrawal = useMutation({
-        mutationFn: (token: string) => withdrawal({ accessToken: token }),
-        onSuccess: () => {
-            localStorage.removeItem("token");
-            showAlert("회원탈퇴가 완료되었습니다.");
-            nav("/auth/login");
-        },
-        onError: (error: any) => {
-            console.log(error);
-            showAlert(error.response.data.message);
-        },
-    });
+  const userWithdrawal = (token: string) => {
+    withdrawal({ accessToken: token }).then(
+      () => {
+        localStorage.removeItem('token');
+        showAlert('회원탈퇴가 완료되었습니다.');
+        nav('/auth/login');
+      },
+      (error) => {
+        console.log(error);
+        showAlert(error.response.data.message);
+      }
+    );
+  };
 
-    const userChangeNickname = useMutation({
-        mutationFn: (data: ChangeNickNameProps) => changeNickname(data),
-        onSuccess: (res: ChangeNickNameResponse) => {
-            showAlert("변경이 완료되었습니다.");
-            const { userNickname, userGoal } = res;
-            useAuthStore.setState({ userNickname, userGoal });
-            queryClient.invalidateQueries({ queryKey: ["userNickname"] }); // 닉네임 및 목표 변경 후 사용자 정보를 다시 가져옴
-        },
-        onError: (error: any) => {
-            console.log(error);
-            showAlert(error.response.data.message);
-        },
-    });
+  const userChangeNickname = useMutation({
+    mutationFn: (data: ChangeNickNameProps) => changeNickname(data),
+    onSuccess: (res: ChangeNickNameResponse) => {
+      showAlert('변경이 완료되었습니다.');
+      const { userNickname, userGoal } = res;
+      useAuthStore.setState({ userNickname, userGoal });
+      queryClient.invalidateQueries({ queryKey: ['userNickname'] }); // 닉네임 및 목표 변경 후 사용자 정보를 다시 가져옴
+    },
+    onError: (error: any) => {
+      console.log(error);
+      showAlert(error.response.data.message);
+    },
+  });
 
-    return {
-        userSignup: userSignup.mutate,
-        userLogin: userLogin.mutate,
-        userChangePassword: userChangePassword.mutate,
-        userWithdrawal: userWithdrawal.mutate,
-        userLogout: userLogout.mutate,
-        userChangeNickname: userChangeNickname.mutate,
-        userNickname,
-        userGoal,
-        bookReading,
-        bookFinished,
-        bookFinishedCount,
-        bookReadingCount,
-        getMainData,
-        isFinishEmpty,
-        isReadingEmpty,
-    };
+  return {
+    userSignup,
+    userLogin,
+    userChangePassword,
+    userWithdrawal,
+    userLogout,
+    userChangeNickname: userChangeNickname.mutate,
+    userNickname,
+    userGoal,
+    bookReading,
+    bookFinished,
+    bookFinishedCount,
+    bookReadingCount,
+    getMainData,
+    isFinishEmpty,
+    isReadingEmpty,
+  };
 };
