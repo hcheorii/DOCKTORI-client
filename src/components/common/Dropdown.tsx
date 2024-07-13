b96dd2030096669b748b6e5f268a2fc8b7e84413
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-07-14 02:24:20 +0900

    fix : useAuth 리팩토링

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -1,117 +1,117 @@
-import { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
 
 interface Props {
-  children: React.ReactNode;
-  toggleButton: React.ReactNode;
-  isOpen?: boolean;
+    children: React.ReactNode;
+    toggleButton: React.ReactNode;
+    isOpen?: boolean;
 }
 
 function DropDown({ children, toggleButton, isOpen = false }: Props) {
-  const [open, setOpen] = useState(isOpen);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+    const [open, setOpen] = useState(isOpen);
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    function handleOutsideClick(event: MouseEvent) {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setOpen(false);
-      }
-    }
+    useEffect(() => {
+        function handleOutsideClick(event: MouseEvent) {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target as Node)
+            ) {
+                setOpen(false);
+            }
+        }
 
-    document.addEventListener('mousedown', handleOutsideClick);
+        document.addEventListener("mousedown", handleOutsideClick);
 
-    return () => {
-      document.removeEventListener('mousedown', handleOutsideClick);
-    };
-  }, [dropdownRef]);
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        };
+    }, [dropdownRef]);
 
-  return (
-    <DropDownStyle $open={open} ref={dropdownRef}>
-      <button className="toggle" onClick={() => setOpen(!open)}>
-        {toggleButton}
-      </button>
-      {open && (
-        <div className="panel" onClick={() => setOpen(false)}>
-          {children}
-        </div>
-      )}
-    </DropDownStyle>
-  );
+    return (
+        <DropDownStyle $open={open} ref={dropdownRef}>
+            <button className="toggle" onClick={() => setOpen(!open)}>
+                {toggleButton}
+            </button>
+            {open && (
+                <div className="panel" onClick={() => setOpen(false)}>
+                    {children}
+                </div>
+            )}
+        </DropDownStyle>
+    );
 }
 
 interface DropDownStyleProps {
-  $open: boolean;
+    $open: boolean;
 }
 
 const DropDownStyle = styled.div<DropDownStyleProps>`
-  button {
-    background: none;
-    border: none;
-    cursor: pointer;
-    outline: none;
-  }
-
-  .toggle {
-    svg {
-      width: 30px;
-      height: 30px;
-      fill: ${({ theme, $open }) =>
-        $open ? theme.color.primary : theme.color.text};
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
+        outline: none;
     }
-    svg:hover {
-      fill: ${({ theme }) => theme.color.background};
+
+    .toggle {
+        svg {
+            width: 30px;
+            height: 30px;
+            fill: ${({ theme, $open }) =>
+                $open ? theme.color.primary : theme.color.text};
+        }
+        svg:hover {
+            fill: ${({ theme }) => theme.color.background};
+        }
     }
-  }
 
-  .panel {
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    bottom: 40px;
-    right: 0;
-    width: 200px;
-    padding: 10px;
-    background-color: ${({ theme }) => theme.color.background};
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    border-radius: ${({ theme }) => theme.borderRadius.default};
-    z-index: 100;
+    .panel {
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        bottom: 40px;
+        right: 0;
+        width: 200px;
+        padding: 10px;
+        background-color: ${({ theme }) => theme.color.background};
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        border-radius: ${({ theme }) => theme.borderRadius.default};
+        z-index: 100;
 
-    @media (max-width: 1200px) {
-      width: 50px;
+        @media (max-width: 1200px) {
+            width: 50px;
+        }
     }
-  }
 
-  .panel > * {
-    display: flex;
-    gap: 3px;
-    text-align: center;
-    align-items: center;
-    padding: 10px;
-    font-size: 1.3rem;
-    text-decoration: none;
-    color: black;
-    border-radius: ${({ theme }) => theme.borderRadius.default};
+    .panel > * {
+        display: flex;
+        gap: 3px;
+        text-align: center;
+        align-items: center;
+        padding: 10px;
+        font-size: 1.3rem;
+        text-decoration: none;
+        color: black;
+        border-radius: ${({ theme }) => theme.borderRadius.default};
 
-    @media (max-width: 1200px) {
-      justify-content: center;
-      padding: 10px 0;
+        @media (max-width: 1200px) {
+            justify-content: center;
+            padding: 10px 0;
+        }
     }
-  }
 
-  .panel > *:hover {
-    background-color: #be8a62;
-    color: #fff;
-    cursor: pointer;
-  }
-
-  .panel > * > span {
-    @media (max-width: 1200px) {
-      display: none;
+    .panel > *:hover {
+        background-color: #be8a62;
+        color: #fff;
+        cursor: pointer;
     }
-  }
+
+    .panel > * > span {
+        @media (max-width: 1200px) {
+            display: none;
+        }
+    }
 `;
 
 export default DropDown;
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,102 +1,99 @@
-import { removeToken, useAuthStore } from '../store/authStore';
-import { useAlert } from './useAlert';
-import { useNavigate } from 'react-router-dom';
+import { removeToken, useAuthStore } from "../store/authStore";
+import { useAlert } from "./useAlert";
+import { useNavigate } from "react-router-dom";
 import {
-  changepassword,
-  login,
-  logout,
-  signup,
-  withdrawal,
-} from '../api/auth.api';
-import { SignupInfo, User, UserChangePassword } from '../models/user.model';
+    changepassword,
+    login,
+    logout,
+    signup,
+    withdrawal,
+} from "../api/auth.api";
+import { SignupInfo, User, UserChangePassword } from "../models/user.model";
 
 export const useAuth = () => {
-  const { storeLogin, storeLogout } = useAuthStore();
-  const { showAlert } = useAlert();
-  const nav = useNavigate();
+    const { storeLogin, storeLogout } = useAuthStore();
+    const { showAlert } = useAlert();
+    const nav = useNavigate();
 
-  const userLogin = (data: User) => {
-    login(data).then(
-      (res) => {
-        storeLogin(res.accessToken);
-        showAlert('로그인이 성공했습니다.');
-        nav('/main');
-      },
-      (error) => {
-        console.log(error);
-        showAlert(
-          error.response?.data?.message || '로그인 중 오류가 발생했습니다.'
+    const handleApiCall = async <T>(
+        apiCall: () => Promise<T>,
+        successMessage: string | null,
+        errorMessage: string,
+        callback?: (res: T) => void
+    ) => {
+        try {
+            const res = await apiCall();
+            if (successMessage) showAlert(successMessage);
+            if (callback) callback(res);
+        } catch (error: any) {
+            console.log(error);
+            showAlert(error.response?.data?.message || errorMessage);
+        }
+    };
+
+    const userLogin = async (data: User) => {
+        await handleApiCall(
+            () => login(data),
+            "로그인이 성공했습니다.",
+            "로그인 중 오류가 발생했습니다.",
+            (res) => {
+                storeLogin(res.accessToken);
+                nav("/main");
+            }
         );
-      }
-    );
-  };
+    };
 
-  const userSignup = (data: SignupInfo) => {
-    signup(data).then(
-      () => {
-        showAlert('회원가입이 완료되었습니다.');
-        nav('/auth/login');
-      },
-      (error) => {
-        console.log(error);
-        showAlert(
-          error.response?.data?.message || '회원가입 중 오류가 발생했습니다.'
+    const userSignup = async (data: SignupInfo) => {
+        await handleApiCall(
+            () => signup(data),
+            "회원가입이 완료되었습니다.",
+            "회원가입 중 오류가 발생했습니다.",
+            () => nav("/auth/login")
         );
-      }
-    );
-  };
+    };
 
-  const userLogout = () => {
-    logout().then(
-      () => {
-        storeLogout();
-        nav('/auth/login');
-      },
-      (error) => {
-        console.log(error);
-        showAlert(error.message || '로그아웃 중 오류가 발생했습니다.');
-      }
-    );
-  };
-
-  const userChangePassword = (data: UserChangePassword) => {
-    changepassword(data).then(
-      () => {
-        showAlert('비밀번호 변경이 완료되었습니다.\n다시 로그인 하세요.');
-        localStorage.removeItem('token');
-        nav('/auth/login');
-      },
-      (error) => {
-        console.log(error);
-        showAlert(
-          error.response?.data?.message ||
-            '비밀번호 변경 중 오류가 발생했습니다.'
+    const userLogout = async () => {
+        await handleApiCall(
+            () => logout(),
+            null,
+            "로그아웃 중 오류가 발생했습니다.",
+            () => {
+                storeLogout();
+                nav("/auth/login");
+            }
         );
-      }
-    );
-  };
+    };
 
-  const userWithdrawal = (token: string) => {
-    withdrawal().then(
-      () => {
-        localStorage.removeItem('token');
-        storeLogout();
-        nav('/auth/login');
-      },
-      (error) => {
-        console.log(error);
-        showAlert(
-          error.response?.data?.message || '회원탈퇴 중 오류가 발생했습니다.'
+    const userChangePassword = async (data: UserChangePassword) => {
+        await handleApiCall(
+            () => changepassword(data),
+            "비밀번호 변경이 완료되었습니다.\n다시 로그인 하세요.",
+            "비밀번호 변경 중 오류가 발생했습니다.",
+            () => {
+                localStorage.removeItem("token");
+                nav("/auth/login");
+            }
         );
-      }
-    );
-  };
+    };
 
-  return {
-    userSignup,
-    userLogin,
-    userChangePassword,
-    userWithdrawal,
-    userLogout,
-  };
+    const userWithdrawal = async () => {
+        await handleApiCall(
+            () => withdrawal(),
+            null,
+            "회원탈퇴 중 오류가 발생했습니다.",
+            () => {
+                localStorage.removeItem("token");
+                storeLogout();
+                nav("/auth/login");
+            }
+        );
+    };
+
+    return {
+        userSignup,
+        userLogin,
+        userChangePassword,
+        userWithdrawal,
+        userLogout,
+    };
 };
